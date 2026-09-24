15b8f782cb6f82d486a82ffe0f09a034646a7eff
MasonTheBag <masonthebag@gmail.com>
2026-09-24 12:10:57 -0400

    some UI and random item stuff started

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,94 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [Header("Item Panel")]
+    [SerializeField] private GameObject itemsPanel;
+
+
+    [Header("Random Items & Upgrades")]
+    [SerializeField] private GameObject upgradePanel;
+    private string[] itemsName = {"option 1", "option 2", "option 3", "option 4", "option 5", "option 6"};
+    private string[] itemsDescription = { "option 1 description", "option 2 description", "option 3 description", "option 4 description", "option 5 description", "option 6 description" };
+
+
+
+
+
     void Start()
     {
+
+        // Turns off panel on game start
+        itemsPanel.SetActive(false);
+        upgradePanel.SetActive(false);
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        // Toggles player item inventory on tab hold
+        if (Keyboard.current.tabKey.wasPressedThisFrame)
+        {
+
+            itemsPanel.SetActive(true);
+
+        }
+
+        if (Keyboard.current.tabKey.wasReleasedThisFrame)
+        {
+
+            itemsPanel.SetActive(false);
+
+        }
+
     }
+
+
+    // Logic to pick three random upgrades and fill them into the appropriate spot on the upgrade panel 
+    private void RandomItemPicker()
+    {
+
+
+
+
+
+        upgradePanel.SetActive(true);
+
+
+
+
+
+
+
+    }
+
+    // Sends the picked item from above and sends the information to the player items panel
+    private void ItemSelected()
+    {
+
+
+
+
+    }
+
+    // Skips items selection
+    public void ItemSkip()
+    {
+
+        upgradePanel.SetActive(false);
+
+    }
+
+
+
+
+
+
+
+
+
 }
---
+++ Assets/Scripts/Scripts_UI/UI Manager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+
+    [Header("UI Panels")]
+    [SerializeField] private GameObject filler;
+
+    private void Start()
+    {
+
+        // Set panels to off on game start
+
+
+    }
+
+
+    // BUTTONS FOR MENUS & PANEL INTERACTIONS
+    // ---------------------------------------
+
+
+
+
+
+
+}
